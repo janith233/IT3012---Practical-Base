de15c6b6a60fa6b2fa100d70cb979041f7024efa
janith233 <janithpriyankara233@gmail.com>
2026-07-25 15:41:25 +0530

    Practical 1
    Added functionality for toxic traps that decrease score upon collision.

--- visual_grid_game.py
+++ visual_grid_game.py
@@ -26,6 +26,15 @@ class VisualGridHuntGame:
             if pos_tuple != (0, 0) and pos_tuple not in self.walls:
                 self.food_positions.add(pos_tuple)
 
+        #Declaring new trap collcetion
+        self.toxic_traps = set()
+        while len(self.toxic_traps) < num_opponents:
+            fx = random.randint(0, self.width - 1)
+            fy = random.randint(0, self.height - 1)
+            pos_tuple = (gx, gy)
+            if pos_tuple != (0, 0) and pos_tuple not in self.walls and pos_tuple not in self.food_positions:
+                self.toxic_traps.add(pos_tuple)
+
         # Generate adversarial opponents
         self.opponents = []
         while len(self.opponents) < num_opponents:
@@ -47,7 +56,8 @@ class VisualGridHuntGame:
             'hit_wall': tuple(self.agent_pos) in self.walls,
             'collision': self.collision,
             'score': self.score,
-            'remaining_food': len(self.food_positions)
+            'remaining_food': len(self.food_positions),
+            'smells_toxin': tuple(self.agent_pos) in self.toxic_traps
         }
 
     def execute_action(self, action: str):
@@ -65,6 +75,11 @@ class VisualGridHuntGame:
 
         if tuple(new_pos) in self.walls:
             self.score -= 5
+        else:
+            self.agent_pos = new_pos
+
+        if tuple(new_pos) in self.toxic_traps:
+            self.score -= 15
         else:
             self.agent_pos = new_pos
 
@@ -145,6 +160,13 @@ class GridGameGUI:
             y1 = (self.env.height - 1 - fy) * self.cell_size + offset
             self.canvas.create_oval(x1, y1, x1 + self.cell_size * 0.5, y1 + self.cell_size * 0.5, fill="#f59e0b",
                                     outline="#d97706")
+
+        for gx, gy in self.toxic_traps:
+            offset = self.cell_size * 0.25
+            x1 = gx * self.cell_size + offset
+            y1 = (self.env.height - 1 - gy) * self.cell_size + offset
+            self.canvas.create_oval(x1, y1, x1 + self.cell_size * 0.5, y1 + self.cell_size * 0.5, fill="#800080",
+                                    outline="#cea2fd")
 
         for ox, oy in self.env.opponents:
             offset = self.cell_size * 0.2
